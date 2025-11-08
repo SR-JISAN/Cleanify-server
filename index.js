fd366b00ee8c0ef0b38a469c8239dadd01110977
SR-JISAN <dev.md.jisan@gmail.com>
2025-11-09 00:38:06 +0600

    set mongo DB

--- index.js
+++ index.js
@@ -1,15 +1,55 @@
+require("dotenv").config();
 const express = require("express");
-const cors = require('cors');
+const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 // middleWar
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
+// DB_USERS;
+// /DB_PASS
+
+const uri = `mongodb+srv://${process.env.DB_USERS}:${process.env.DB_PASS}@cluster0.tcugfjh.mongodb.net/?appName=Cluster0`;
+
+// mongoDb connect
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    await client.connect();
+    const db = client.db("cleanify-DB");
+    const serviceColl = db.collection("service");
+
+
+    
 
 
 
 
+
+
+
+
+
+
+
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // nothing
+  }
+}
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
@@ -18,3 +58,5 @@ app.get("/", (req, res) => {
 app.listen(port, () => {
   console.log(`cleanify app listening on port ${port}`);
 });
+
+run().catch(console.dir);
