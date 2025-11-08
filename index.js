5ef5978a0b07ef8d29228a0984660a35bf1be44e
SR-JISAN <dev.md.jisan@gmail.com>
2025-11-08 23:50:27 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const cors = require('cors');
+const app = express();
+const port = 5000;
+
+// middleWar
+app.use(cors())
+app.use(express.json())
+
+
+
+
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`cleanify app listening on port ${port}`);
+});
