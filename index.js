0e007d3e18f97e8bb611d3dbb408eee43f5e66b2
SR-JISAN <dev.md.jisan@gmail.com>
2025-11-13 22:24:32 +0600

    verify

--- index.js
+++ index.js
@@ -1,15 +1,43 @@
-require("dotenv").config();
 const express = require("express");
+require('dotenv').config();
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, Collection, ObjectId } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 5000;
+
+const admin = require("firebase-admin");
+
+const serviceAccount = require("./admin-key.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});
+
 
 // middleWar
 app.use(cors());
 app.use(express.json());
-// DB_USERS;
-// /DB_PASS
+const verifyToken = async(req,res,next)=>{
+   if (!req.headers.authorization){
+      return res.status(401).send({message:"UnAuthorized Access"})
+   } 
+   const token = req.headers.authorization.split(' ')[1]
+    try {
+      
+      const userInfo =  await admin.auth().verifyIdToken(token);
+      req.token_owner = userInfo.email
+       next();
+    } catch {
+      return res.status(401).send({ message: "UnAuthorized Access" });
+      
+    }
+
+
+
+
+}
+
+
 
 const uri = `mongodb+srv://${process.env.DB_USERS}:${process.env.DB_PASS}@cluster0.tcugfjh.mongodb.net/?appName=Cluster0`;
 
@@ -27,7 +55,91 @@ async function run() {
   try {
     await client.connect();
     const db = client.db("cleanify-DB");
-    const serviceColl = db.collection("service");
+    const issueContributeColl = db.collection("issueContribute");
+    const issuesColl = db.collection("issues")
+
+    // ---------------Issues Collection------------
+    
+     app.get("/issuesLimit", async (req, res) => {
+       const cursor = issuesColl.find().sort({date: -1}).limit(6);
+       const result = await cursor.toArray()
+       res.send(result);
+     });
+
+      app.get("/issueDetails/:id", async (req, res) => {
+        const id = req.params.id;
+        const query = { _id: new ObjectId(id) };
+        const result = await issuesColl.findOne(query);
+        res.send(result);
+      });
+      app.get("/issues",verifyToken, async (req, res) => {
+        const email = req.query.email;
+        const query = {};
+        if (email) {
+          if (email !== req.token_owner) {
+            return res.status(403).send({ message: "Forbidden Access" });
+          }
+          query.email = email;
+        }
+        const cursor = issuesColl.find(query);
+        const result = await cursor.toArray();
+        res.send(result);
+      });
+     
+    app.post('/issues',async(req,res)=>{
+        const newIssue = req.body;
+        const result = await  issuesColl.insertOne(newIssue)
+        res.send(result)
+    })
+     
+
+    app.patch('/issues/:id',async(req,res)=>{
+       const id = req.params.id;
+       const query = { _id: new ObjectId(id) };
+       const updateIssue = req.body;
+       const update ={
+        $set: updateIssue
+       }
+       const result = await issuesColl.updateOne(query,update)
+       res.send(result);
+    })
+
+    app.delete('/issues/:id',async(req,res)=>{
+        const id = req.params.id;
+        const query = { _id : new ObjectId(id) }
+        const result = await issuesColl.deleteOne(query)
+        res.send(result)
+    })
+    // ----------Issue-contribution--------------
+
+    app.get("/contributes",verifyToken ,async(req,res)=>{
+        const email = req.query.email
+        const query = {}
+        if(email){
+          if(email !== req.token_owner){
+                return  res.status(403).send({ message: "Forbidden Access" });
+          }
+          query.email = email
+        }
+        const cursor = issueContributeColl.find(query)
+        const result = await cursor.toArray()
+        res.send(result)
+    });
+
+    app.get('/issues/contributes/:id',async(req,res)=>{
+          const id = req.params.id
+          const query = { Contribute_id: id };
+          const cursor = issueContributeColl.find(query).sort({amount: -1});
+          const result = await cursor.toArray()
+          res.send(result)
+    })
+
+    app.post('/contributes',async(req,res)=>{
+        const newContribute =req.body
+        const result = await issueContributeColl.insertOne(newContribute)
+        res.send(result)
+    })
+     
 
 
     
